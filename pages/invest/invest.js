a72a1d5eeb62b16b241ae4e9fd49dacd16a3b868
liujiabao <liujiabao@qihaiyun.com>
2020-05-30 17:53:15 +0800

    ljb bug...........

--- pages/invest/invest.js
+++ pages/invest/invest.js
@@ -9,18 +9,39 @@ Page({
       }
     }
   },
+  onLoad: function () {
+
+    var that = this;
+
+    /**
+     * 获取当前设备的宽高
+     */
+    wx.getSystemInfo({
+
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });
+  },
   /**
    * 页面的初始数据
    */
   data: {
+    currentTab:0,
+    winWidth: 0,
+    winHeight: 0,
     selected: [{
-      date: '2018-5-21'
+      date: '2020-5-21'
     }, {
-      date: '2018-5-22'
+      date: '2020-5-22'
     }, {
-      date: '2018-5-24'
+      date: '2020-5-24'
     }, {
-      date: '2018-5-25'
+      date: '2020-5-25'
     }]
   },
   /**
@@ -36,5 +57,25 @@ Page({
     let time = e.detail;
     console.log(time)
 
-  }
+  },
+  //  tab切换逻辑
+  swichNav: function (e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+
+  bindChange: function (e) {
+
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
+  },
 })
